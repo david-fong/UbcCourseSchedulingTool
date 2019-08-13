ec1b7354ea4a5a647809e1f4c8ae0c53c7ea732e
david-fong <davidfong19@gmail.com>
2019-08-13 13:29:45 -0700

    add IDE run configurations and javafx dependency

--- Core/source/org/bse/core/CoreMain.java
+++ Core/source/org/bse/core/CoreMain.java
@@ -1,12 +1,19 @@
 package org.bse.core;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Runs the UbcSchedulerTool Application.
  */
-public final class CoreMain {
+public class CoreMain extends Application {
 
     public static void main(String[] args) {
-        ;
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
 }
